Fixed the scaling of the branches

project-1/rel_energy_Gamow.py
@@ -28,7 +28,7 @@ for i in range(N_steps):
 	star = EnergyProduction(T[i], rho_sun)
 	star.run_all_cycles()
 
-	E_tot = star.PP0 + star.PP1 + np.sum(star.PP2) + np.sum(star.PP3) + star.CNO
+	E_tot = 3 * star.PP0 + star.PP1 + np.sum(star.PP2) + np.sum(star.PP3) + star.CNO
 
 	E_rel[0, i] = star.PP0 / E_tot							# PP0 rel. energy 		(Excluded from plot as it is common for all PP branches)
 	E_rel[1, i] = (star.PP0 + star.PP1) / E_tot				# PP1 rel. energy