00d69950f6df89593205be6674d811a037914c4f
JohanCarlsen <johan_carlsen@hotmail.com>
2023-02-27 09:38:11 +0100

    Split rel. energy and Gamow peak into two programs

--- project-1/rel_energy.py
+++ project-1/rel_energy.py
@@ -1,7 +1,6 @@
 '''
 In this program we will calculate and plot the relative energy
-production from each of the PP branches and the CNO cycle, and
-the Gamow peak for all the reactions.
+production from each of the PP branches and the CNO cycle.
 '''
 import numpy as np 
 import matplotlib.pyplot as plt 
@@ -9,15 +8,12 @@ from energy_production import EnergyProduction
 
 '''
 For the relative energy we will use temperature from 10^4 to 10^9 K.
-For the Gamow peak we will use energy from 10^-17 to 10^-13 J.
 '''
 rho_sun = 1.62e5	# Sun density [kgm^-3]
 N_steps = 1001 		# No. of points to solve for 
 
-T = np.logspace(4, 9, N_steps)		# Temperature range [K]
-E = np.logspace(-17, -13, N_steps)	# Energy range [J]
-
-E_rel = np.zeros((5, N_steps))		# Array to contain the rel. energies
+T = np.logspace(4, 9, N_steps)			# Temperature range [K]
+E_rel = np.zeros((5, N_steps))			# Array to contain the rel. energies
 
 for i in range(N_steps):
 	'''
@@ -31,9 +27,9 @@ for i in range(N_steps):
 	E_tot = 2 * star.PP0 + star.PP1 + np.sum(star.PP2) + np.sum(star.PP3) + star.CNO
 
 	E_rel[0, i] = star.PP0 / E_tot							# PP0 rel. energy 		(Excluded from plot as it is common for all PP branches)
-	E_rel[1, i] = (star.PP0 + star.PP1) / E_tot			# PP1 rel. energy
+	E_rel[1, i] = (star.PP0 + star.PP1) / E_tot				# PP1 rel. energy
 	E_rel[2, i] = (star.PP0 + np.sum(star.PP2)) / E_tot		# PP2 rel. energy
-	E_rel[3, i] = (np.sum(star.PP3)) / E_tot		# PP3 rel. energy
+	E_rel[3, i] = (np.sum(star.PP3)) / E_tot				# PP3 rel. energy
 	E_rel[4, i] = star.CNO / E_tot							# CNO rel. energy
 
 plt.figure(figsize=(10, 4))
